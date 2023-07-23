34e9133bb1750fd629414dde398883c08ed11666
ayacocoa <shanyi2020@qq.com>
2023-07-23 13:27:22 +0800

    0

--- src/utils/env.js
+++ src/utils/env.js
@@ -15,8 +15,7 @@ if (process.env.NODE_ENV === "development") {
   baseUrl = "http://localhost:3000";
   baseImgPath = "http://localhost:3000";
 } else {
-  baseUrl =
-    "https://wall-backend-44781-8-1317899860.sh.run.tcloudbase.com:3000";
+  baseUrl = "https://wall-backend-44781-8-1317899860.sh.run.tcloudbase.com";
   baseImgPath = "";
 }
 console.log(baseUrl);
